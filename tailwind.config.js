db3443e137af347d38e08faae3deeeca9175c60f
sirwin6 <irwins06@gmail.com>
2023-01-03 15:16:44 -0500

    Home page general layout

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      iphone: '390px',
+      // => @media (min-width: 640px) { ... }
+
+      tablet: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
